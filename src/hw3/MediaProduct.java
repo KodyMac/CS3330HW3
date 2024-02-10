59ec7e280922309def0393cebd6bf8565f5c8409
Kody McNamara <Kody McNamara@10.7.15.193>
2024-02-10 12:30:06 -0600

    IDK

--- src/hw3/MediaProduct.java
+++ src/hw3/MediaProduct.java
@@ -1,5 +1,53 @@
 package hw3;
 
 public class MediaProduct {
-
+		//attributes
+	protected String title;
+	protected double price;
+	protected int year;
+	protected Genre genre;
+	
+		//Constructor
+	public MediaProduct(String title, double price, int year, Genre genre) {
+		this.title=title;
+		this.price=price;
+		this.year=year;
+		this.genre=genre;
+	}
+	
+	
+	
+	
+		//getters and setters
+	protected String getTitle() {
+		return title;
+	}
+	
+	protected void setTitle(String title) {
+		this.title=title;
+	}
+	
+	protected double getPrice() {
+		return price;
+	}
+	
+	protected void setPrice(double price) {
+		this.price=price;
+	}
+	
+	protected int getYear() {
+		return year;
+	}
+	
+	protected void setYear(int year) {
+		this.year=year;
+	}
+	
+	protected Genre getGenre() {
+		return genre;
+	}
+	
+	protected void setGenre(Genre genre) {
+		this.genre=genre;
+	}
 }
